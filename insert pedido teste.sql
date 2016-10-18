0334e563ef484815fc7f0cca9147619613bf417e
senadiego <diegosena_93@hotmail.com>
2016-10-18 04:17:20 -0200

    insert pedido teste

---
+++ insert pedido teste.sql
@@ -0,0 +1,14 @@
+INSERT INTO `tblPedido`
+( `tipoPedido`, `dtEntrega`, `dtCompra`, `codCliente`, `codStatus`, `codVeiculoTransp`) 
+VALUES 
+('ganho de massa' , now(), now(), null, null , null);
+
+INSERT INTO `tblPedido`
+( `tipoPedido`, `dtEntrega`, `dtCompra`, `codCliente`, `codStatus`, `codVeiculoTransp`) 
+VALUES 
+('força e resistencia' , now(), now(),null, null , null);
+
+INSERT INTO `tblPedido`
+( `tipoPedido`, `dtEntrega`, `dtCompra`, `codCliente`, `codStatus`, `codVeiculoTransp`) 
+VALUES 
+('perda de peso' , now(), now(), null, null , null);
