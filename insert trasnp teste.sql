b662822c3f3a762b72e4f1631953f08c4b1b8749
senadiego <diegosena_93@hotmail.com>
2016-10-21 21:39:54 -0200

    insert transportadora script

---
+++ insert trasnp teste.sql
@@ -0,0 +1,15 @@
+INSERT INTO `tblTransportadora`
+(`nomeTransportadora`, `cnpjTransportadora`, `emailTransportadora`, `telefoneTransportadora`, `responsavelTransportadora`, `codEndereco`)
+VALUES 
+('trasnp 1','1234','trasnp@trasnp1','41419865','resp transp1',null);
+
+INSERT INTO `tblTransportadora`
+(`nomeTransportadora`, `cnpjTransportadora`, `emailTransportadora`, `telefoneTransportadora`, `responsavelTransportadora`, `codEndereco`)
+VALUES 
+('trasnp 2','1234','trasnp@trasnp2','41419865','resp transp2',null);
+
+
+INSERT INTO `tblTransportadora`
+(`nomeTransportadora`, `cnpjTransportadora`, `emailTransportadora`, `telefoneTransportadora`, `responsavelTransportadora`, `codEndereco`)
+VALUES 
+('trasnp 3','1234','trasnp@trasnp3','41419865','resp transp3',null);
