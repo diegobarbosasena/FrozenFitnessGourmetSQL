fc31bf1edd62c6077257e949eb821ae656a4945c
senadiego <diegosena_93@hotmail.com>
2016-10-21 21:55:23 -0200

    a

--- insert transp teste.sql
+++ insert transp teste.sql
@@ -1,15 +1,15 @@
 INSERT INTO `tblTransportadora`
 (`nomeTransportadora`, `cnpjTransportadora`, `emailTransportadora`, `telefoneTransportadora`, `responsavelTransportadora`, `codEndereco`)
 VALUES 
-('trasnp 1','1234','trasnp@trasnp1','41419865','resp transp1',null);
+('transp 1','1234','transp@transp1','41419865','resp transp1',null);
 
 INSERT INTO `tblTransportadora`
 (`nomeTransportadora`, `cnpjTransportadora`, `emailTransportadora`, `telefoneTransportadora`, `responsavelTransportadora`, `codEndereco`)
 VALUES 
-('trasnp 2','1234','trasnp@trasnp2','41419865','resp transp2',null);
+('transp 2','1234','transp@transp2','41419865','resp transp2',null);
 
 
 INSERT INTO `tblTransportadora`
 (`nomeTransportadora`, `cnpjTransportadora`, `emailTransportadora`, `telefoneTransportadora`, `responsavelTransportadora`, `codEndereco`)
 VALUES 
-('trasnp 3','1234','trasnp@trasnp3','41419865','resp transp3',null);
+('transp 3','1234','transp@transp3','41419865','resp transp3',null);
