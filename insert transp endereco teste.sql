ec600fd08a0d74b703a7e432aae888babdba4e8c
DIEGO BARBOSA SENA <15160055@CFP127ALUNO.LOCAL>
2016-10-25 16:59:38 -0200

    atualizacao

--- insert transp endereco teste.sql
+++ insert transp endereco teste.sql
@@ -11,15 +11,16 @@ VALUES
 ('transp 1', '123456', 'transp1@transp1.com', '456789', 'resp transp 1', last_insert_id());
 
 
-select 
-	t.codTransportadora, t.cnpjTransportadora, t.emailTransportadora, t.telefoneTransportadora, t.responsavelTransportadora,
-    e.logradouro, e.cep, e.numero, e.bairro, e.complemento
-from 
-	tblTransportadora as t
-inner join
-	tblEndereco as e
-on 
-	(t.codEndereco = e.codEndereco);
+SELECT 
+	t.codTransportadora, t.nomeTransportadora, t.cnpjTransportadora, t.emailTransportadora, t.telefoneTransportadora, t.responsavelTransportadora, 
+	e.codEndereco, e.logradouro, e.cep, e.numero, e.bairro, e.complemento 
+FROM 
+	tblTransportadora AS t 
+INNER JOIN tblEndereco AS e 
+ON 
+	(t.codEndereco = e.codEndereco) 
+ORDER BY 
+	codTransportadora DESC;
 	
 
 
