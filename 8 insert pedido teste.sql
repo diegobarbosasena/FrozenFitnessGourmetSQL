e20307dca4c643e60ed85eaa37bc5b7483ca4ae7
senadiego <diegosena_93@hotmail.com>
2016-11-07 22:30:04 -0200

    atulizacao mais uma

--- 8 insert pedido teste.sql
+++ 8 insert pedido teste.sql
@@ -10,16 +10,16 @@ VALUES
 INSERT INTO `tblPedido`
 (`tipoPedido`, `dtEntrega`, `dtCompra`, `codCliente`, `codStatus`, `codVeiculoTransp`) 
 VALUES 
-('pedido 2', now() + INTERVAL 5 DAY, now()  , 2, 1, 2);
+('pedido 2', now() + INTERVAL 5 DAY, now()  , 2, 1, null);
 
 /*pedido 3 teste*/
 INSERT INTO `tblPedido`
 (`tipoPedido`, `dtEntrega`, `dtCompra`, `codCliente`, `codStatus`, `codVeiculoTransp`) 
 VALUES 
-('pedido 3', now() + INTERVAL 5 DAY , now()  , 3, 1, 3);
+('pedido 3', now() + INTERVAL 5 DAY , now()  , 3, 1, null);
 
 /*pedido 4 teste*/
 INSERT INTO `tblPedido`
 (`tipoPedido`, `dtEntrega`, `dtCompra`, `codCliente`, `codStatus`, `codVeiculoTransp`) 
 VALUES 
-('pedido 4', now() + INTERVAL 5 DAY, now()  , 4, 1, 4);
+('pedido 4', now() + INTERVAL 5 DAY, now()  , 4, 1, null);
