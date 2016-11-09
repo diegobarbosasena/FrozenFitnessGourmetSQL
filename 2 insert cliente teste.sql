cf6e056fbcf4ed4011e115504a1903c30b310113
senadiego <diegosena_93@hotmail.com>
2016-11-08 22:31:24 -0200

    cliente sexo

--- 2 insert cliente teste.sql
+++ 2 insert cliente teste.sql
@@ -2,25 +2,25 @@ USE dbsmartgourmet;
 
 
 INSERT INTO `tblCliente`
-	(`nomeCliente`, `cpfCliente`, `dtNascCliente`, `peso`, `altura`, `telefoneCliente`, `celularCliente`, `emailCliente`) 
+	(`nomeCliente`, `cpfCliente`, `dtNascCliente`, `peso`, `altura`, `telefoneCliente`, `celularCliente`, `emailCliente`, `sexo`) 
 VALUES 
-	('cliente 1','111111','1990-10-02','75.2','1.75', '111111', '111111', 'cliente1@cliente.com');
+	('cliente 1','111111','1990-10-02','75.2','1.75', '111111', '111111', 'cliente1@cliente.com','M');
 
 
 INSERT INTO `tblCliente`
-	(`nomeCliente`, `cpfCliente`, `dtNascCliente`, `peso`, `altura`, `telefoneCliente`, `celularCliente`, `emailCliente`) 
+	(`nomeCliente`, `cpfCliente`, `dtNascCliente`, `peso`, `altura`, `telefoneCliente`, `celularCliente`, `emailCliente`, `sexo`) 
 VALUES 
-	('cliente 2','222222','1989-11-22','80.2','1.82', '222222', '222222', 'cliente2@cliente.com');
+	('cliente 2','222222','1989-11-22','80.2','1.82', '222222', '222222', 'cliente2@cliente.com','F');
 
 
 INSERT INTO `tblCliente`
-	(`nomeCliente`, `cpfCliente`, `dtNascCliente`, `peso`, `altura`, `telefoneCliente`, `celularCliente`, `emailCliente`) 
+	(`nomeCliente`, `cpfCliente`, `dtNascCliente`, `peso`, `altura`, `telefoneCliente`, `celularCliente`, `emailCliente`, `sexo`) 
 VALUES 
-	('cliente 3','333333','1984-04-11','65.0','1.68', '333333', '333333', 'cliente3@cliente.com');
+	('cliente 3','333333','1984-04-11','65.0','1.68', '333333', '333333', 'cliente3@cliente.com','F');
 
 
 INSERT INTO `tblCliente`
-	(`nomeCliente`, `cpfCliente`, `dtNascCliente`, `peso`, `altura`, `telefoneCliente`, `celularCliente`, `emailCliente`) 
+	(`nomeCliente`, `cpfCliente`, `dtNascCliente`, `peso`, `altura`, `telefoneCliente`, `celularCliente`, `emailCliente`, `sexo`) 
 VALUES 
-	('cliente 4','444444','1992-05-20','73.0','1.78', '444444', '444444', 'cliente4@cliente.com');
+	('cliente 4','444444','1992-05-20','73.0','1.78', '444444', '444444', 'cliente4@cliente.com','M');
 
