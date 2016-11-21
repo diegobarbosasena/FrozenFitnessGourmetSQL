425d5f2639942cf24bdc217e96f00aef31d52972
DIEGO BARBOSA SENA <15160055@CFP127ALUNO.LOCAL>
2016-11-21 17:02:58 -0200

    pedido

--- 8 insert pedido teste.sql
+++ 8 insert pedido teste.sql
@@ -1,25 +1,21 @@
 USE dbsmartgourmet;
 
-/*pedido 1 teste*/
 INSERT INTO `tblPedido`
-(`tipoPedido`, `dtEntrega`, `dtCompra`, `codCliente`, `codStatus`, `codVeiculoTransp`) 
+	(`tipoPedido`, `dtEntrega`, `dtCompra`, `total`, `codCliente`, `codStatus`, `codVeiculoTransp`) 
 VALUES 
-('pedido 1', now() + INTERVAL 5 DAY, now()  , 1, 1, null);
+	('web', now() + INTERVAL 5 DAY, now() , '500.00', 1, 1, null);
 
-/*pedido 2 teste*/
 INSERT INTO `tblPedido`
-(`tipoPedido`, `dtEntrega`, `dtCompra`, `codCliente`, `codStatus`, `codVeiculoTransp`) 
+	(`tipoPedido`, `dtEntrega`, `dtCompra`, `total`, `codCliente`, `codStatus`, `codVeiculoTransp`) 
 VALUES 
-('pedido 2', now() + INTERVAL 5 DAY, now()  , 2, 1, null);
+	('telefone', now() + INTERVAL 5 DAY, now() , '254.24', 2, 1, null);
 
-/*pedido 3 teste*/
 INSERT INTO `tblPedido`
-(`tipoPedido`, `dtEntrega`, `dtCompra`, `codCliente`, `codStatus`, `codVeiculoTransp`) 
+	(`tipoPedido`, `dtEntrega`, `dtCompra`, `total`, `codCliente`, `codStatus`, `codVeiculoTransp`) 
 VALUES 
-('pedido 3', now() + INTERVAL 5 DAY , now()  , 3, 1, null);
+	('web', now() + INTERVAL 5 DAY, now() , '684.85', 3, 1, null);
 
-/*pedido 4 teste*/
 INSERT INTO `tblPedido`
-(`tipoPedido`, `dtEntrega`, `dtCompra`, `codCliente`, `codStatus`, `codVeiculoTransp`) 
+	(`tipoPedido`, `dtEntrega`, `dtCompra`, `total`, `codCliente`, `codStatus`, `codVeiculoTransp`) 
 VALUES 
-('pedido 4', now() + INTERVAL 5 DAY, now()  , 4, 1, null);
+	('telefone', now() + INTERVAL 5 DAY, now() , '21.10', 4, 1, null);
