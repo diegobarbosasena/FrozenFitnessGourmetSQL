57b94aeff558cd627b2051a8b633a957ae2882fd
juliapcosta97 <juliapcosta97@gmail.com>
2016-11-30 15:49:12 -0200

    insert produtos

---
+++ insert_categoriaMateria.sql
@@ -0,0 +1,9 @@
+
+
+insert into tblCategoriaMateria (nomeCategoriaMateria) values("Frios");
+insert into tblCategoriaMateria (nomeCategoriaMateria) values("Farinha");
+insert into tblCategoriaMateria (nomeCategoriaMateria) values("Carne");
+insert into tblCategoriaMateria (nomeCategoriaMateria) values("Frutos do Mar");
+insert into tblCategoriaMateria (nomeCategoriaMateria) values("Salada");
+insert into tblCategoriaMateria (nomeCategoriaMateria) values("Sobremesa");
+insert into tblCategoriaMateria (nomeCategoriaMateria) values("Suco");
---
+++ insert_produtos.sql
@@ -0,0 +1,44 @@
+
+insert into tblProduto (nomeProduto, precoProduto, descricaoProduto, caloriaProduto, valorEnergeticoProduto, carboidratoProduto,
+proteinaProduto, sodioProduto, gordurasProduto, imagemProduto) values ('Frango Grelhado', '10', 'Frango grelhado com o melhor tempero 
+caseiro.', '20', '20', '20', '20', '20', '20', 'conteudo/imagem/frango_grelhado.jpg');
+insert into tblCatProduto (codCategoriaMateria, codProduto) values ('3', LAST_INSERT_ID());
+
+insert into tblProduto (nomeProduto, precoProduto, descricaoProduto, caloriaProduto, valorEnergeticoProduto, carboidratoProduto,
+proteinaProduto, sodioProduto, gordurasProduto, imagemProduto) values ('Batata Doce', '10', 'Batata Doce', '20', '20', '20', '20', '20', '20', 'conteudo/imagem/batata.jpg');
+insert into tblCatProduto (codCategoriaMateria, codProduto) values ('2', LAST_INSERT_ID());
+
+insert into tblProduto (nomeProduto, precoProduto, descricaoProduto, caloriaProduto, valorEnergeticoProduto, carboidratoProduto,
+proteinaProduto, sodioProduto, gordurasProduto, imagemProduto) values ('Arroz Integral', '10', 'Arroz Integral', '20', '20', '20', '20', '20', '20', 'conteudo/imagem/arroz.jpg');
+insert into tblCatProduto (codCategoriaMateria, codProduto) values ('1', LAST_INSERT_ID());
+
+insert into tblProduto (nomeProduto, precoProduto, descricaoProduto, caloriaProduto, valorEnergeticoProduto, carboidratoProduto,
+proteinaProduto, sodioProduto, gordurasProduto, imagemProduto) values ('Carne Assada', '10', 'Carne Assada', '20', '20', '20', '20', '20', '20', 'conteudo/imagem/carne.jpg');
+insert into tblCatProduto (codCategoriaMateria, codProduto) values ('1', LAST_INSERT_ID());
+
+insert into tblProduto (nomeProduto, precoProduto, descricaoProduto, caloriaProduto, valorEnergeticoProduto, carboidratoProduto,
+proteinaProduto, sodioProduto, gordurasProduto, imagemProduto) values ('Lasanha ', '10', 'Lasanha', '20', '20', '20', '20', '20', '20', 'conteudo/imagem/lasanha.jpg');
+insert into tblCatProduto (codCategoriaMateria, codProduto) values ('1', LAST_INSERT_ID());
+
+insert into tblProduto (nomeProduto, precoProduto, descricaoProduto, caloriaProduto, valorEnergeticoProduto, carboidratoProduto,
+proteinaProduto, sodioProduto, gordurasProduto, imagemProduto) values ('Feijão', '10', 'Feijão', '20', '20', '20', '20', '20', '20', 'conteudo/imagem/feijão.jpg');
+insert into tblCatProduto (codCategoriaMateria, codProduto) values ('1', LAST_INSERT_ID());
+
+
+insert into tblProduto (nomeProduto, precoProduto, descricaoProduto, caloriaProduto, valorEnergeticoProduto, carboidratoProduto,
+proteinaProduto, sodioProduto, gordurasProduto, imagemProduto) values ('Salada', '10', 'Salada', '20', '20', '20', '20', '20', '20', 'conteudo/imagem/salada.jpg');
+insert into tblCatProduto (codCategoriaMateria, codProduto) values ('1', LAST_INSERT_ID());
+
+
+insert into tblProduto (nomeProduto, precoProduto, descricaoProduto, caloriaProduto, valorEnergeticoProduto, carboidratoProduto,
+proteinaProduto, sodioProduto, gordurasProduto, imagemProduto) values ('Salmão Assada', '10', 'Salmão Assada', '20', '20', '20', '20', '20', '20', 'conteudo/imagem/salmao.jpg');
+insert into tblCatProduto (codCategoriaMateria, codProduto) values ('1', LAST_INSERT_ID());
+
+
+insert into tblProduto (nomeProduto, precoProduto, descricaoProduto, caloriaProduto, valorEnergeticoProduto, carboidratoProduto,
+proteinaProduto, sodioProduto, gordurasProduto, imagemProduto) values ('Macarrão', '10', 'Macarrão', '20', '20', '20', '20', '20', '20', 'conteudo/imagem/macarrao.jpg');
+insert into tblCatProduto (codCategoriaMateria, codProduto) values ('1', LAST_INSERT_ID());
+
+insert into tblProduto (nomeProduto, precoProduto, descricaoProduto, caloriaProduto, valorEnergeticoProduto, carboidratoProduto,
+proteinaProduto, sodioProduto, gordurasProduto, imagemProduto) values ('Farofa', '10', 'Farofa', '20', '20', '20', '20', '20', '20', 'conteudo/imagem/farofa.jpg');
+insert into tblCatProduto (codCategoriaMateria, codProduto) values ('1', LAST_INSERT_ID());
